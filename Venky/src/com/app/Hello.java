0169dfec553fb0720b7fe14509dfdcfa21674df2
rs <rs@DESKTOP-V0HG1DG>
2018-09-05 22:01:50 +0530

    2 commit

--- Venky/src/com/app/Hello.java
+++ Venky/src/com/app/Hello.java
@@ -3,6 +3,14 @@ package com.app;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("HELLO!!!");
+		
+		System.out.println("v");
+		System.out.println("e");
+		System.out.println("n");
+		System.out.println("k");
+		System.out.println("y");
+		
+		System.out.println("HAI!!");
 	}
 }
