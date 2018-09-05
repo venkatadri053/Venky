0a2aea7f89bb1a86401efda9d3ef9b0a3e9a545a
rs <rs@DESKTOP-V0HG1DG>
2018-09-05 22:34:03 +0530

    aszc

--- Venky/src/com/app/Hello.java
+++ Venky/src/com/app/Hello.java
@@ -3,23 +3,6 @@ package com.app;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO!!!");
-		int a=10;
-		int b=15;
-		int c=a+b;
-	System.out.println("sum="+c);
-		
-		System.out.println("V");
-		System.out.println("E");
-		System.out.println("N");
-		System.out.println("K");
-		System.out.println("A");
-		System.out.println("T");
-
-		System.out.println("0");
-		System.out.println("5");
-		System.out.println("3");
-		
-		System.out.println("HAI!!");
+		System.out.println("hai!!");
 	}
 }
